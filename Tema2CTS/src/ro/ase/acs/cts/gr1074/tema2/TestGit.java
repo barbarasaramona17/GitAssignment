d4674b223306b4f3e7fbac3cbf61bb7be568b8bb
Ramona <Ramona@192.168.1.100>
2020-03-18 14:13:08 +0200

    First commit

---
+++ Tema2CTS/src/ro/ase/acs/cts/gr1074/tema2/TestGit.java
@@ -0,0 +1,9 @@
+package ro.ase.acs.cts.gr1074.tema2;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git ! Denumirea proiectului de licenta este Aplicatie mobila pentru managementul comenzilor în cadrul unui restaurant");
+	}
+
+}
