61dae100d9fa63edaf84348b30506c1e0f38b734
Ramona <Ramona@192.168.1.100>
2020-03-18 14:22:20 +0200

    A fost adaugata clasa Restaurant

---
+++ Tema2CTS/src/ro/ase/acs/cts/gr1074/tema2/Restaurant.java
@@ -0,0 +1,16 @@
+package ro.ase.acs.cts.gr1074.tema2;
+
+public class Restaurant {
+	private String denumire;
+	private String adresa;
+	private int nrAngajati;
+	
+	public Restaurant(String denumire, String adresa, int nrAngajati) {
+		super();
+		this.denumire = denumire;
+		this.adresa = adresa;
+		this.nrAngajati = nrAngajati;
+	}
+	
+	
+}
